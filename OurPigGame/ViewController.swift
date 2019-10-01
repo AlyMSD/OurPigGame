70f58767267473b25849bf5406bc1dbf18c1b9c8
Snigdha Mishra <snigdha.mishra.066@k12.friscoisd.org>
2019-10-01 14:19:39 -0500

    commit

--- OurPigGame/ViewController.swift
+++ OurPigGame/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var dice: UIImageView!
+    
     @IBOutlet weak var label: UILabel!
-    var player1 = 0
-    var player2 = 0
     
     @IBOutlet weak var rollDice: UIButton!
+    
+    var player1Score = 0
+    var player2Score = 0
+    var player1Points = 0
+    var player2Points = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,9 +30,24 @@ class ViewController: UIViewController {
     @IBAction func diceRolled(_ sender: Any) {
         let min : UInt32 = 1
         let max : UInt32 = 7
-        let randomNumber = arc4random_uniform(max - min)+min
-        label.text = String("Rolled a \(randomNumber)")
+        let diceRolledNumber = arc4random_uniform(max - min)+min
+        label.text = String("Rolled a \(diceRolledNumber)")
+        switch diceRolledNumber{
+        case 1: dice.image = UIImage(named: "diceSide1")
+        case 2: dice.image = UIImage(named: "diceSide2")
+        case 3: dice.image = UIImage(named: "diceSide3")
+        case 4: dice.image = UIImage(named: "diceSide4")
+        case 5: dice.image = UIImage(named: "diceSide5")
+        case 6: dice.image = UIImage(named: "diceSide6")
+        default: dice.image = UIImage(named: "diceSide1")
+            
+        let continueGame = (player1Score<24)&&(player2Score<24)
+        while continueGame{
+            
+            }
+        }
     }
+    
     
 }
 
