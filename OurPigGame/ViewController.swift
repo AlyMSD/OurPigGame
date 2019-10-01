9a70fc1ffd5794070b4ab261918656807b539d7b
aly damani <alymuhammad.damani.144@k12.friscoisd.org>
2019-10-01 13:20:16 -0500

    I added random gen

--- OurPigGame/ViewController.swift
+++ OurPigGame/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var player1 = 0
+    var player2 = 0
+    
+    @IBOutlet weak var rollDice: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func diceRolled(_ sender: Any) {
+        let min : UInt32 = 1
+        let max : UInt32 = 7
+        let randomNumber = arc4random_uniform(max - min)+min
+        label.text = String("Rolled a \(randomNumber)")
+    }
+    
 }
 
